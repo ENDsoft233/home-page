53121643565094a8e0bf297d5aaea8933ce161c7
Ray <i@r-ay.cn>
2021-02-02 16:15:33 +0800

    Update CopyRight information and Links

--- src/components/ButtonList.vue
+++ src/components/ButtonList.vue
@@ -20,24 +20,39 @@ export default {
     return {
       list: [
         {
-          img: '/assets/icon/qq.svg',
-          title: 'QQ群',
-          link: 'https://jq.qq.com/?_wv=1027&k=24hAeviP'
+          img: 'https://pro-video.xiaoheiban.cn/qpn/cea1a68a-1095-411a-ba18-7977ac7b6a47.svg',
+          title: 'BiliBili',
+          link: 'https://space.bilibili.com/24553739'
         },
         {
-          img: '/assets/icon/github.svg',
-          title: 'Github',
-          link: 'https://github.com/mcmyth'
+          img: 'https://pro-video.xiaoheiban.cn/iod/c719a4dc-8343-4cd9-bf64-04d8ff64605b.svg',
+          title: 'GitHub',
+          link: 'https://github.com/ENDsoft233'
         },
         {
-          img: '/assets/icon/codepen.svg',
-          title: 'Codepen',
-          link: 'https://codepen.io/MC_Myth'
+          img: 'https://pro-video.xiaoheiban.cn/veh/f09af134-d8f2-4ecd-82a6-6924db78c8fb.svg',
+          title: 'Steam',
+          link: 'https://SteamCommunity.com/id/ENDsoft233'
         },
         {
-          title: '博客',
+          img: 'https://pro-video.xiaoheiban.cn/tdj/08a5fb5a-dc48-4012-b9f7-2cebc1b185e6.svg',
+          title: 'Twitter',
+          link: 'https://twitter.com/ENDsoft233'
+        },
+        {
+          img: 'https://pro-video.xiaoheiban.cn/hgj/981bafc6-98d5-48c9-877c-b47779aa0dd3.svg',
+          title: 'Zhihu',
+          link: 'https://zhihu.com/people/dtraywong'
+        },
+        {
+          img: 'https://pro-video.xiaoheiban.cn/pyp/5ecb7351-bb70-4f28-a577-a6c5eee0bb16.svg',
+          title: 'Email',
+          link: 'mailto://i@r-ay.cn'
+        },
+        {
           text: 'Blog',
-          link: 'https://blog.mc-myth.cn'
+          title: '博客',
+          link: 'https://blog.r-ay.cn'
         }
       ]
     }
